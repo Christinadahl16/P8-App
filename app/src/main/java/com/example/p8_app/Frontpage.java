903a7a4e923fe3deaba44ecfb156345b34ee617c
Christinadahl16 <Cdahl17@student.aau.dk>
2021-04-19 13:44:36 +0200

    Test

---
+++ app/src/main/java/com/example/p8_app/Frontpage.java
@@ -0,0 +1,11 @@
+package com.example.p8_app;
+
+public interface Frontpage {
+    public String getName() { return Name; }
+
+    public void setName(String Name) { this.Name = Name; }
+
+    public Integer getAge() { return Age; }
+
+    public void setAge(Integer Age) { this.Age = Age; }
+}
