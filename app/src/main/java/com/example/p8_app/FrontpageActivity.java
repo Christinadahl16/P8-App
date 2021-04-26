7b9d637c8f4130b4c6a6cc0263014a4beaee250a
Christinadahl16 <Cdahl17@student.aau.dk>
2021-04-26 14:40:27 +0200

    .

--- app/src/main/java/com/example/p8_app/FrontpageActivity.java
+++ app/src/main/java/com/example/p8_app/FrontpageActivity.java
@@ -1,4 +1,23 @@
 package com.example.p8_app;
 
-public class FrontpageActivity {
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class FrontpageActivity extends AppCompatActivity {
+
+    private Button signout;
+
+    private FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frontpage);
+        signout=(Button)findViewById(R.id.SignOut);
+}
 }
