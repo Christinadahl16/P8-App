85fa48036913331a929958fd7db7cc8507186d58
Christinadahl16 <Cdahl17@student.aau.dk>
2021-04-28 10:55:13 +0200

    Fragmentstyle

--- app/src/main/java/com/example/p8_app/FrontpageActivity.java
+++ app/src/main/java/com/example/p8_app/FrontpageActivity.java
@@ -1,17 +1,63 @@
 package com.example.p8_app;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class FrontpageActivity extends AppCompatActivity {
+    private Bundle savedInstanceState;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_frontpage);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        }
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
 
-}
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.products:
+                            selectedFragment = new ProductsFragment();
+                            break;
+                        case R.id.aboutGrønttorvet:
+                            selectedFragment = new AboutgtFragment();
+                            break;
+                        case R.id.AboutApp:
+                            selectedFragment = new ThisappFragment();
+                            break;
+
+                        case R.id.Myprofile:
+                            selectedFragment = new ProfileFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+
+            };
+
+     {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frontpage);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
+    }
+
+    }
+
+
