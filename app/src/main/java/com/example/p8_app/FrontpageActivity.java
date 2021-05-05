bd1bba4d39d75c4df8b4bfaaccb4fe600022afe0
Christinadahl16 <Cdahl17@student.aau.dk>
2021-05-05 14:50:18 +0200

    .

--- app/src/main/java/com/example/p8_app/FrontpageActivity.java
+++ app/src/main/java/com/example/p8_app/FrontpageActivity.java
@@ -16,7 +16,7 @@ public class FrontpageActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_farmeroverview);
+        setContentView(R.layout.activity_main);
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_nav);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
@@ -49,20 +49,24 @@ public class FrontpageActivity extends AppCompatActivity {
                 }
             };
     public class FarmerOverview extends AppCompatActivity {
+
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_farmeroverview);
+            ;
 
         }
 
         /*Ved de 3 linjer nedenunder har vi lavet en "onclick"-funktion til de 3 knapper fra FarmerOverview.xml*/
         /*Linjen nedenunder hører til "Se udvalg" som henviser til Farmer XXX's udvalg*/
         public void button4(View view) {
-            findViewById(R.id.products);
+
+
         }
         /*Linjen nedenunder hører til "Se udvalg" som henviser til Farmer XXY's udvalg*/
         public void button2(View view) {
+           ;
         }
         /*Linjen nedenunder henviser til knappen "Sign Out", som logger brugeren ud*/
         public void button5(View view) {
