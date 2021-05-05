7c9e1bb7968c56485f63e4e51e4412190bcac5aa
MieJohanne <72790249+MieJohanne@users.noreply.github.com>
2021-05-05 13:09:34 +0200

    hejhej
    hejhej

--- app/src/main/java/com/example/p8_app/FrontpageActivity.java
+++ app/src/main/java/com/example/p8_app/FrontpageActivity.java
@@ -2,6 +2,7 @@ package com.example.p8_app;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -47,4 +48,24 @@ public class FrontpageActivity extends AppCompatActivity {
                     return true;
                 }
             };
+    public class FarmerOverview extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_farmeroverview);
+
+        }
+
+        /*Ved de 3 linjer nedenunder har vi lavet en "onclick"-funktion til de 3 knapper fra FarmerOverview.xml*/
+        /*Linjen nedenunder hører til "Se udvalg" som henviser til Farmer XXX's udvalg*/
+        public void button4(View view) {
+            findViewById(R.id.products);
+        }
+        /*Linjen nedenunder hører til "Se udvalg" som henviser til Farmer XXY's udvalg*/
+        public void button2(View view) {
+        }
+        /*Linjen nedenunder henviser til knappen "Sign Out", som logger brugeren ud*/
+        public void button5(View view) {
+        }
+    }
 }
