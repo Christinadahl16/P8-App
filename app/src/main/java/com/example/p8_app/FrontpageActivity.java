4f6b913d236bf2a6fed00fbadf05466ed9b05d3c
Christinadahl16 <Cdahl17@student.aau.dk>
2021-05-06 13:08:52 +0200

    .

--- app/src/main/java/com/example/p8_app/FrontpageActivity.java
+++ app/src/main/java/com/example/p8_app/FrontpageActivity.java
@@ -1,8 +1,10 @@
 package com.example.p8_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -48,28 +50,61 @@ public class FrontpageActivity extends AppCompatActivity {
                     return true;
                 }
             };
-    public class FarmerOverview extends AppCompatActivity {
+    public class FarmerOverview extends AppCompatActivity implements View.OnClickListener {
+        private Button button2;
+        private Button button4;
+        private Button button5;
+
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_farmeroverview);
-            ;
+            setContentView(R.layout.activity_main);
+
+            button2 = (Button) findViewById(R.id.button2);
+            button4 = (Button) findViewById(R.id.button4);
+            button5 = (Button) findViewById(R.id.button5);
+
+            button2.setOnClickListener(this);
+            button4.setOnClickListener(this);
+            button5.setOnClickListener(this);
+
+
+
+            }
+
+        @Override
+        public void onClick(View v) {
+            openProducts ();
+
+        }
+        public void openProducts() {
+            Intent intent = new Intent(this, ProductsFragment.class);
+            startActivity(intent);
+        }
+    }
+
+       /* @Override
+        public void onClick(View v) {
+
+        });
 
         }
 
         /*Ved de 3 linjer nedenunder har vi lavet en "onclick"-funktion til de 3 knapper fra FarmerOverview.xml*/
         /*Linjen nedenunder hører til "Se udvalg" som henviser til Farmer XXX's udvalg*/
-        public void button4(View view) {
+       /* public void button4(View view) {
+
 
 
         }
         /*Linjen nedenunder hører til "Se udvalg" som henviser til Farmer XXY's udvalg*/
-        public void button2(View view) {
+        /*public void button2(View view) {
            ;
         }
         /*Linjen nedenunder henviser til knappen "Sign Out", som logger brugeren ud*/
-        public void button5(View view) {
+        /*public void button5(View view) {
         }
-    }
+    }*/
 }
+
