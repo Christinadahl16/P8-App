5cf26c9cf97cb84d002281eba8bb22c6eef7a449
Christinadahl16 <Cdahl17@student.aau.dk>
2021-05-10 08:21:24 +0200

    .

--- app/src/main/java/com/example/p8_app/FarmerOverview.java
+++ app/src/main/java/com/example/p8_app/FarmerOverview.java
@@ -1,28 +1,47 @@
 package com.example.p8_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FarmerOverview extends AppCompatActivity {
-@Override
+    private Button button2;
+    private Button button4;
+    private Button button5;
+
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-setContentView(R.layout.activity_farmeroverview);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-}
+        button2 = (Button) findViewById(R.id.button2);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
 
-/*Ved de 3 linjer nedenunder har vi lavet en "onclick"-funktion til de 3 knapper fra FarmerOverview.xml*/
-/*Linjen nedenunder hører til "Se udvalg" som henviser til Farmer XXX's udvalg*/
-    public void button4(View view) {
-        findViewById(R.id.products);
-    }
-/*Linjen nedenunder hører til "Se udvalg" som henviser til Farmer XXY's udvalg*/
-    public void button2(View view) {
-    }
-/*Linjen nedenunder henviser til knappen "Sign Out", som logger brugeren ud*/
-    public void button5(View view) {
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent( FarmerOverview.this, ProductsFragment.class));
+                finish();
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FarmerOverview.this, ProductsFragment.class));
+                finish();
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FarmerOverview.this, LoginActivity.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/p8_app/FrontpageActivity.java
+++ app/src/main/java/com/example/p8_app/FrontpageActivity.java
@@ -1,10 +1,7 @@
 package com.example.p8_app;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -24,6 +21,7 @@ public class FrontpageActivity extends AppCompatActivity {
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
     }
+
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
@@ -46,49 +44,13 @@ public class FrontpageActivity extends AppCompatActivity {
                             break;
                     }
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
-                            selectedFragment). commit();
+                            selectedFragment).commit();
                     return true;
                 }
             };
-    public class FarmerOverview extends AppCompatActivity {
-        private Button button2;
-        private Button button4;
-        private Button button5;
 
-
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-
-            button2 = (Button) findViewById(R.id.button2);
-            button4 = (Button) findViewById(R.id.button4);
-            button5 = (Button) findViewById(R.id.button5);
-
-            button2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    startActivity(new Intent( FrontpageActivity.this, ProductsFragment.class));
-                    finish();
-                }
-            });
-            button4.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    startActivity(new Intent(FrontpageActivity.this, ProductsFragment.class));
-                    finish();
-                }
-            });
-            button5.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    startActivity(new Intent(FrontpageActivity.this, LoginActivity.class));
-                    finish();
-                }
-            });
-        }
     }
-}
+
 
 
 
